b947257ad744fd78efbcd12b44800b4e958020ec
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-03-23 19:25:19 -0500

    missing some video urls

--- src/components/videos/VideoURLs.ts
+++ src/components/videos/VideoURLs.ts
@@ -9,8 +9,8 @@ export const VIDEOS_TITLE_URL: VideoURLObj[] = [
     url: "https://www.youtube.com/embed/fX5U50VGxtg?si=d2oguHaC6vzlqIFY",
   },
   {
-    title: "Postman Twitch Stream",
-    url: "https://www.youtube.com/embed/sfUjJx6yRAI?si=v0q88zH_cVlkSrHq",
+    title: "Visualizing the OAuth Flow and Why PKCE is Needed",
+    url: "https://www.youtube.com/embed/_zWovo2zv6k",
   },
   {
     title:
@@ -18,26 +18,30 @@ export const VIDEOS_TITLE_URL: VideoURLObj[] = [
     url: "https://www.youtube.com/embed/eg7I8x-u0sc?si=H1C7_IVkVNGMA9xi",
   },
   {
-    title: "Visualizing the OAuth Flow and Why PKCE is Needed",
-    url: "https://www.youtube.com/embed/_zWovo2zv6k",
+    title: "OAuth 2.0 Flow: How to Securely Get your Grapes",
+    url: "https://www.youtube.com/embed/-3nwwH9PgHA?si=rr8vc43Fg0sUCHZG",
+  },
+  {
+    title: "OpenID Connect - Continuing the OAuth 2.0 Fruit Analogy",
+    url: "https://www.youtube.com/embed/wYrzc4AWCzw?si=Bwlsuq8xtWLwkqw8",
+  },
+  {
+    title:
+      "Authorize your apps using OAuth 2.0 with Ping: Postman live stream on Twitch",
+    url: "https://www.youtube.com/embed/sfUjJx6yRAI?si=v0q88zH_cVlkSrHq",
   },
   {
     title: "Try PingOne for Customers! You'll be Glad You Did.",
     url: "https://www.youtube.com/embed/ypa8prwEMcc?si=EibB5NHZkPrl7IG6",
   },
   {
-    title: "Learn About the PingOne for Customers Developer Community",
-    url: "https://www.youtube.com/embed/D-yskqXigZQ",
+    title: "Postman and Ping Identity Twitch Stream",
+    url: "https://www.youtube.com/embed/1hs5e7TDwM4",
   },
   {
     title:
       "Ping Identity and Postman Team Up to Show You How to Upload Your API to the API Network",
     url: "https://www.youtube.com/embed/T0F5hqXxGOk",
-  },
-  {
-    title:
-      "Don't Make Things Harder By Wearing Too Many Hats, PingOne for Customers Can Handle It",
-    url: "https://www.youtube.com/embed/PJMjHLzD_Kg",
   },
   {
     title:
@@ -49,16 +53,34 @@ export const VIDEOS_TITLE_URL: VideoURLObj[] = [
     url: "https://www.youtube.com/embed/PeZ-Fz_soFs",
   },
   {
-    title: "Postman and Ping Identity Twitch Stream",
-    url: "https://www.youtube.com/embed/1hs5e7TDwM4",
-  },
-  {
     title: "One Remote for all of your CIAM Solutions",
     url: "https://www.youtube.com/embed/2Mkzy3ZWT6Q",
   },
   {
+    title: "PingOne for Customers Postman Collection",
+    url: "https://www.youtube.com/embed/O9lQGwKTEQM?si=2gyrAXf2PgkDfXiy",
+  },
+  {
+    title: "Meet Your App Launch Deadlines While Also Making It Secure",
+    url: "https://www.youtube.com/embed/3yCKQ5b0st8?si=cD1vMEAvTJ60qADh",
+  },
+  {
+    title:
+      "Don't Make Things Harder By Wearing Too Many Hats, PingOne for Customers Can Handle It",
+    url: "https://www.youtube.com/embed/PJMjHLzD_Kg",
+  },
+  {
     title: "A Quick Guide on the PingOne for Customers Administrative Console",
     url: "https://www.youtube.com/embed/fCbW1YFnm6M",
+  },
+  {
+    title:
+      "Dynamic Authentication Policy with MFA Triggered on a Specified Attribute Value",
+    url: "https://www.youtube.com/embed/9QOpDGLLxIQ?si=zgAnVIm7UBrfulUf",
+  },
+  {
+    title: "Learn About the PingOne for Customers Developer Community",
+    url: "https://www.youtube.com/embed/D-yskqXigZQ",
   },
   {
     title: "Developer's Playlist",
