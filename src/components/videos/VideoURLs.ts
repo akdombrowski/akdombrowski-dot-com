326dbecbfaa8458ee453b1d0918405a186cba970
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-03-30 14:06:38 -0500

    sorted tags, added one

--- src/components/videos/VideoURLs.ts
+++ src/components/videos/VideoURLs.ts
@@ -8,66 +8,66 @@ export const VIDEOS_TITLE_URL: VideoURLObj[] = [
   {
     title: "OAuth 2.0: Implicit, Authorization Code, and PKCE",
     url: "https://www.youtube.com/embed/fX5U50VGxtg?si=d2oguHaC6vzlqIFY",
-    tag: ["education", "oauth", "implicit", "authzcode", "pkce"],
+    tag: ["authzcode", "education", "implicit", "oauth", "pkce", "standards"],
   },
   {
     title: "Visualizing the OAuth Flow and Why PKCE is Needed",
     url: "https://www.youtube.com/embed/_zWovo2zv6k",
-    tag: ["education", "oauth", "pkce", "standards", "iam"],
+    tag: ["education", "iam", "oauth", "pkce", "standards"],
   },
   {
     title:
       "What are the OAuth 2.0 Grant Type Flows? First up, Authorization Code",
     url: "https://www.youtube.com/embed/eg7I8x-u0sc?si=H1C7_IVkVNGMA9xi",
-    tag: ["education", "oauth", "authzcode", "standards", "iam"],
+    tag: ["authzcode", "education", "iam", "oauth", "standards"],
   },
   {
     title: "OAuth 2.0 Flow: How to Securely Get your Grapes",
     url: "https://www.youtube.com/embed/-3nwwH9PgHA?si=rr8vc43Fg0sUCHZG",
-    tag: ["education", "oauth", "authz", "standards", "iam"],
+    tag: ["authz", "education", "iam", "oauth", "standards"],
   },
   {
     title: "OpenID Connect - Continuing the OAuth 2.0 Fruit Analogy",
     url: "https://www.youtube.com/embed/wYrzc4AWCzw?si=Bwlsuq8xtWLwkqw8",
-    tag: ["education", "oidc", "authn", "standards", "iam"],
+    tag: ["authn", "education", "iam", "oidc", "standards"],
   },
   {
     title:
       "Authorize your apps using OAuth 2.0 with Ping: Postman live stream on Twitch",
     url: "https://www.youtube.com/embed/sfUjJx6yRAI?si=v0q88zH_cVlkSrHq",
     tag: [
-      "education",
-      "tools",
-      "postman",
       "collab",
-      "twitch",
-      "live",
-      "stream",
-      "standards",
+      "education",
       "iam",
+      "live",
+      "postman",
+      "standards",
+      "stream",
+      "tools",
+      "twitch",
     ],
   },
   {
     title: "Try PingOne for Customers! You'll be Glad You Did.",
     url: "https://www.youtube.com/embed/ypa8prwEMcc?si=EibB5NHZkPrl7IG6",
-    tag: ["education", "awareness"],
+    tag: ["awareness", "education"],
   },
   // {
   //   title: "Postman and Ping Identity Twitch Stream",
   //   url: "https://www.youtube.com/embed/1hs5e7TDwM4",
-  //   tag: ["education", ""],
+  //   tag: ["education","",],
   // },
   {
     title:
       "Ping Identity and Postman Team Up to Show You How to Upload Your API to the API Network",
     url: "https://www.youtube.com/embed/T0F5hqXxGOk",
-    tag: ["education", "awareness", "how-to"],
+    tag: ["awareness", "education", "how to"],
   },
   {
     title:
       "Use Postman Environments to Speed Up and Make API Development Easier",
     url: "https://www.youtube.com/embed/IdtBkGQg2Q8",
-    tag: ["education", "postman", "how-to"],
+    tag: ["education", "how to", "postman"],
   },
   {
     title: "The Great Developer Race",
@@ -82,7 +82,7 @@ export const VIDEOS_TITLE_URL: VideoURLObj[] = [
   {
     title: "PingOne for Customers Postman Collection",
     url: "https://www.youtube.com/embed/O9lQGwKTEQM?si=2gyrAXf2PgkDfXiy",
-    tag: ["how-to", "postman"],
+    tag: ["how to", "postman"],
   },
   {
     title: "Meet Your App Launch Deadlines While Also Making It Secure",
@@ -98,7 +98,7 @@ export const VIDEOS_TITLE_URL: VideoURLObj[] = [
   {
     title: "A Quick Guide on the PingOne for Customers Administrative Console",
     url: "https://www.youtube.com/embed/fCbW1YFnm6M",
-    tag: ["education", "how-to"],
+    tag: ["education", "how to"],
   },
   {
     title:
