77f92914825c18ed9cdd3be9dd72ec80f091b940
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-04-15 21:22:26 -0500

    fixed my logo image

--- src/components/appbar/AppBarLogo.tsx
+++ src/components/appbar/AppBarLogo.tsx
@@ -6,29 +6,15 @@ export default function AppBarLogo() {
   return (
     <Box
       id="appbarlogo-boxContainer"
-      display="flex"
-      maxWidth="100%"
-      minHeight="50px"
-      maxHeight="100%"
-      justifyContent="left"
-      alignItems="stretch"
-      position="relative"
-      sx={{ aspectRatio: 16 / 9 }}
     >
-      {/* <Box
-        sx={{ aspectRatio: 16 / 9 }}
-        flexBasis="max-content"
-        position="relative"
-      > */}
       {/* Logo Image */}
       <Image
         src={"/gokart-R.png"}
         alt="logo of anthony dombrowski"
         style={{ overflow: "hidden", objectFit: "contain" }}
-        width={50}
-        height={89}
+        width={73}
+        height={50}
       />
-      {/* </Box> */}
     </Box>
   );
 }
