a4c60ad57a36648a6f78716f44f1d4acb14ea816
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-04-22 13:51:18 -0500

    error pages including global-error (for root layout errors)

---
+++ src/app/(content)/error.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Unstable_Grid2";
+import Typography from "@mui/material/Typography";
+
+export default function ContentErrorPage({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <Container
+      id="errorPage"
+      maxWidth={false}
+      disableGutters
+    >
+      <Grid
+        container
+        spacing={3}
+        justifyContent="center"
+        alignItems="center"
+        pt={2}
+      >
+        <Grid
+          xs={12}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h4">Hey!</Typography>
+        </Grid>
+        <Grid
+          xs={12}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h4">What's that over there 👉?!?!?!</Typography>
+        </Grid>
+
+        <Grid
+          xs={12}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h4">Oh, you just missed it!</Typography>
+        </Grid>
+        <Grid
+          xs={12}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h4">Wait!</Typography>
+        </Grid>
+        <Grid
+          xs={12}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h4">Now, it's over there 👈 !!</Typography>
+        </Grid>
+        <Grid
+          xs={12}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Button
+            variant="outlined"
+            onClick={() => reset()}
+          >
+            <Typography
+              variant="subtitle2"
+              color="cyan"
+            >
+              If you just want to go back, then click me...
+            </Typography>
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
---
+++ src/app/global-error.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Unstable_Grid2";
+import Typography from "@mui/material/Typography";
+
+import { GoogleTagManager } from "@next/third-parties/google";
+
+export default function GlobalErrorPage({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/anthonyProfilesq/favicon.ico"
+          sizes="any"
+        />
+      </head>
+      <GoogleTagManager gtmId="GTM-MJZSL46" />
+      <body>
+        <Container
+          id="errorPage"
+          maxWidth={false}
+          disableGutters
+        >
+          <Grid
+            container
+            spacing={3}
+            justifyContent="center"
+            alignItems="center"
+            pt={2}
+          >
+            <Grid
+              xs={12}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Typography variant="h4">Hey!</Typography>
+            </Grid>
+            <Grid
+              xs={12}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Typography variant="h4">
+                What's that over there 👉?!?!?!
+              </Typography>
+            </Grid>
+
+            <Grid
+              xs={12}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Typography variant="h4">Oh, you just missed it!</Typography>
+            </Grid>
+            <Grid
+              xs={12}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Typography variant="h4">Wait!</Typography>
+            </Grid>
+            <Grid
+              xs={12}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Typography variant="h4">Now, it's over there 👈 !!</Typography>
+            </Grid>
+            <Grid
+              xs={12}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Button
+                variant="outlined"
+                onClick={() => reset()}
+              >
+                <Typography
+                  variant="subtitle2"
+                  color="cyan"
+                >
+                  If you just want to go back, then click me...
+                </Typography>
+              </Button>
+            </Grid>
+          </Grid>
+        </Container>
+      </body>
+    </html>
+  );
+}
