5b87c88f66c0e8d95af87c11b8edab88b3e8ddf4
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-08-15 16:50:39 -0500

    a few more tests

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -45,6 +45,8 @@ const crushed = (
   return false;
 };
 
+const checkCreativity = (name: string) => {};
+
 /**
  * The function `hireAnthonyDombrowski` determines the hiring outcome based on
  * responsibilities and whether the candidate was hired, with potential
@@ -89,7 +91,95 @@ const hireAnthonyDombrowski = (
   return hiringOutcome;
 };
 
-const anthonysWorkHistory = () => {};
+/**
+ * The function `checkCandidateCreativity` generates a random creativity level for
+ * a candidate, with a special case for a candidate named "Anthony Dombrowski"
+ * having maximum creativity level.
+ * @param {string} candidateName - The `candidateName` parameter in the
+ * `checkCandidateCreativity` function is a string that represents the name of a
+ * candidate whose creativity level is being checked.
+ * @returns The `checkCandidateCreativity` function returns the creativity level
+ * of a candidate. If the candidate's name is "Anthony Dombrowski", the creativity
+ * level is set to the maximum possible value (`Number.MAX_VALUE`). Otherwise, a
+ * random creativity level between 0 and 100 is generated and returned.
+ */
+const checkCandidateCreativity = (candidateName: string) => {
+  const creativity_levels = ["meh", "ok", "unbelievable"];
+  const rnd = Math.random() * 100 * 2;
+  let creativityLvl = creativity_levels[rnd % 2];
+
+  if (candidateName === "Anthony Dombrowski") {
+    // creativityLvl = Number.MAX_VALUE;
+    creativityLvl = creativity_levels[2];
+  }
+
+  return creativityLvl;
+};
+
+/**
+ * The function `getSoftwareEngineerExp` returns the string "2017 - 2019 @ Ping
+ * Identity".
+ * @returns The function `getSoftwareEngineerExp` returns the string "2017 - 2019
+ * @ Ping Identity".
+ */
+const getSoftwareEngineerExp = () => {
+  return "2017 - 2019 @ Ping Identity";
+};
+
+/**
+ * The function `getLeadDeveloperAdvocate` returns the string "2019 - 2021 @ Ping
+ * Identity".
+ * @returns The function `getLeadDeveloperAdvocate` returns the string "2019 -
+ * 2021 @ Ping Identity".
+ */
+const getLeadDeveloperAdvocateExp = () => {
+  return "2019 - 2021 @ Ping Identity";
+};
+
+/**
+ * The function `getProductManagerOfDevEx` returns the string "2021 - 2023 @ Ping
+ * Identity".
+ * @returns The function `getProductManagerOfDevEx` returns the string "2021 -
+ * 2023 @ Ping Identity".
+ */
+const getProductManagerOfDevExExp = () => {
+  return "2021 - 2023 @ Ping Identity";
+};
+
+/**
+ * The function `getDevExConsultant` returns the string "2024 @ SpruceID".
+ * @returns "2024 @ SpruceID"
+ */
+const getDevExConsultantExp = () => {
+  return "2024 @ SpruceID";
+};
+
+/**
+ * The function `getAnthonysDeveloperExperience` retrieves Anthony's history in
+ * various roles related to software engineering and developer experience.
+ * @returns An array containing the experiences in the field of software
+ * development and developer experience of Anthony is being returned.
+ */
+const getAnthonysDeveloperExperience = () => {
+  const history: string[] = [];
+
+  const softwareEngineer = getSoftwareEngineerExp();
+
+  const leadDeveloperAdvocate = getLeadDeveloperAdvocateExp();
+
+  const productManagerOfDevEx = getProductManagerOfDevExExp();
+
+  const devExConsultant = getDevExConsultantExp();
+
+  history.push(
+    softwareEngineer,
+    leadDeveloperAdvocate,
+    productManagerOfDevEx,
+    devExConsultant,
+  );
+
+  return history;
+};
 
 function Resume({}: Props) {
   return (
