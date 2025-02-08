b302b51aee62d248ca7b12ca9a1f8d5e99e5bcac
Anthony Dombrowski <akdflyinvee73@gmail.com>
2025-02-08 10:21:35 -0600

    upgrade pkgs wakatime badge nested link fix

--- src/components/WakaTimeBadge.tsx
+++ src/components/WakaTimeBadge.tsx
@@ -6,17 +6,26 @@ import Link from "next/link";
 import Box from "@mui/material/Box";
 
 import Badge from "https://wakatime.com/badge/user/0ff1bf94-98b4-465f-8b63-a51fb5151092.svg";
+import type { EventHandler, MouseEventHandler, SyntheticEvent } from "react";
 
 export default function WakaTimeBadge() {
+  const handleClick: MouseEventHandler<HTMLDivElement> = (
+    event: SyntheticEvent<HTMLDivElement>
+  ): void => {
+    event.preventDefault();
+    window.open("https://wakatime.com/@adombrowski", "_blank");
+  };
+
   return (
     <Box
       p={0}
       display="flex"
       justifyContent="center"
-      component={Link}
       position="relative"
-      target="_blank"
-      href="https://wakatime.com/@adombrowski"
+      // component={Link}
+      // target="_blank"
+      // href="https://wakatime.com/@adombrowski"
+      onClick={handleClick}
     >
       <Badge />
     </Box>
