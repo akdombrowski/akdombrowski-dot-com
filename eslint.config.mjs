b27aafe4c0de45a3fc2a86b34cf3356c32a2f1a9
akdombrowski <akdflyinvee73@gmail.com>
2025-04-05 13:31:47 -0500

    minor change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,4 +10,5 @@ const compat = new FlatCompat({
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
 });
-export default [...compat.extends("next/core-web-vitals", "next/typescript")];
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+export default eslintConfig;
