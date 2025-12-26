97cb1b10026a37304089ae92c6ddb78121b22f15
akdombrowski <akdflyinvee73@gmail.com>
2025-12-26 16:42:38 -0600

    fixed video quilt layout for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,173 @@
-import LandingPage from "@/components/home/LandingPage";
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
-export default function Home() {
-  return <LandingPage />;
+import Roles from "@/components/Roles";
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+
+import TOC from "@/components/home/TOC";
+import SectionDivider from "@/components/home/SectionDivider";
+import VideosQuilt from "@/components/videos/VideosQuilt";
+
+export default function HomePage() {
+  return (
+    <Container
+      id="landing-container"
+      maxWidth={false}
+      disableGutters
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "stretch",
+        height: "100%",
+      }}
+    >
+      <Grid
+        id="landing-gridContainer"
+        container
+        rowSpacing={{ xs: 0 }}
+      >
+        <Grid
+          id="landing-headerGridWrapper"
+          container
+          size={{ xs: 12 }}
+          justifyContent="center"
+          alignItems="center"
+          display="flex"
+          sx={{ outline: "2rem ridge #0400ff2e" }}
+          zIndex={1}
+        >
+          <Hero
+            variant="title"
+            id="landing-hero"
+            size={{ xs: 9 }}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            p={0}
+          />
+
+          <SectionDivider
+            gridProps={{
+              id: "landing-divider1",
+              size: { xs: 12 },
+              paddingTop: 0,
+              paddingBottom: 0,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              zIndex: "1",
+            }}
+            dividerProps={{ sx: { borderColor: "#11004A", borderWidth: "5px", width: "100%" } }}
+          />
+
+          <Grid
+            id="landing-rolesGridWrapper"
+            size={{ xs: 12 }}
+            paddingTop={1}
+            paddingBottom={0}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Roles />
+          </Grid>
+        </Grid>
+
+        <Grid
+          id="landing-rolesDividerWrapper"
+          size={{ xs: 12 }}
+          paddingY={0}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          zIndex={100}
+        >
+          <Divider
+            sx={{
+              zIndex: "100",
+              borderColor: "#11004A",
+              borderWidth: "1px",
+              width: "100%",
+            }}
+          />
+        </Grid>
+
+        <Grid
+          id="landing-badgesDividerWrapper"
+          size={{ xs: 12 }}
+          pt={0}
+          pb={2}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Divider sx={{ borderColor: "#11004A", borderWidth: "3px", width: "100%" }} />
+        </Grid>
+
+        <Grid
+          id="landing-videosQuiltWrapper"
+          container
+          spacing={1}
+          size={12}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <VideosQuilt />
+        </Grid>
+
+        <Grid
+          id="landing-videsoQuiltDivider"
+          size={{ xs: 12 }}
+          paddingY={2}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Divider sx={{ borderColor: "#11004A", borderWidth: "1px", width: "100%" }} />
+        </Grid>
+
+        <Grid
+          id="landing-tocWrapper"
+          container
+          spacing={1}
+          size={12}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <TOC />
+        </Grid>
+
+        <Grid
+          id="landing-videosDivider"
+          size={{ xs: 12 }}
+          paddingY={0}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Divider sx={{ borderColor: "#11004A", borderWidth: "1px", width: "100%" }} />
+        </Grid>
+
+        <Grid
+          id="footerPadding"
+          size={12}
+        >
+          <Box></Box>
+        </Grid>
+
+        <Grid
+          id="landing-footerGridWrapper"
+          size={{ xs: 12 }}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
--- src/components/videos/VideosQuilt.tsx
+++ src/components/videos/VideosQuilt.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import VIDEOS_TITLE_URL from "@/components/videos/VideoURLs";
 import _ from "lodash";
 import VideoContainer from "./VideoContainer";
+
 function srcset(image: string, size: number, rows = 1, cols = 1) {
   return {
     src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
@@ -14,8 +15,10 @@ function srcset(image: string, size: number, rows = 1, cols = 1) {
 }
 
 export default function VideosQuilt() {
-  const cols = 100;
-  const rows = 6;
+  const cols = 3;
+  const numVids = VIDEOS_TITLE_URL.length;
+  const rows = Math.ceil(numVids / cols);
+
   return (
     <Container
       id="videosQuilt-container"
@@ -26,15 +29,16 @@ export default function VideosQuilt() {
         sx={{ width: "100%", height: "auto" }}
         variant="quilted"
         cols={cols}
-        rowHeight={rows * 40}
-        gap={3}
+        rowHeight={"auto"}
+        gap={0}
       >
         {/* {itemData.map((item) => ( */}
         {VIDEOS_TITLE_URL.map((vid, i) => (
           <ImageListItem
             key={i}
-            cols={cols / 2 + (_.random(1024) % cols) / 2}
-            rows={_.random(1024) % rows + 1}
+            // cols={cols / 2 + (_.random(1024) % cols) / 2}
+            cols={1}
+            rows={1}
           >
             <VideoContainer video={vid} />
           </ImageListItem>
