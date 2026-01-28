ef00c0e7af358e2f03a8df38ff5647f606aac467
Anthony Dombrowski <akdflyinvee73@gmail.com>
2026-01-28 17:45:36 -0600

    dynamic imagelist

--- src/components/videos/VideosQuilt.tsx
+++ src/components/videos/VideosQuilt.tsx
@@ -1,11 +1,14 @@
+"use client";
+
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import Container from "@mui/material/Container";
+import { useMediaQuery, useTheme } from "@mui/material";
 
-import Image from "next/image";
-import VIDEOS_TITLE_URL from "@/components/videos/VideoURLs";
 import _ from "lodash";
-import VideoContainer from "./VideoContainer";
+
+import VIDEOS_TITLE_URL from "@/components/videos/VideoURLs";
+import VideoContainer from "@/components/videos/VideoContainer";
 
 function srcset(image: string, size: number, rows = 1, cols = 1) {
   return {
@@ -15,28 +18,28 @@ function srcset(image: string, size: number, rows = 1, cols = 1) {
 }
 
 export default function VideosQuilt() {
-  const cols = 3;
-  const numVids = VIDEOS_TITLE_URL.length;
-  const rows = Math.ceil(numVids / cols);
+  const theme = useTheme();
+  const isSmall = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMedium = useMediaQuery(theme.breakpoints.down("md"));
+  const isLarge = useMediaQuery(theme.breakpoints.down("lg"));
+
+  const cols = isSmall ? 1 : isMedium ? 2 : isLarge ? 3 : 4;
 
   return (
     <Container
       id="videosQuilt-container"
-      maxWidth={"xl"}
+      maxWidth={false}
     >
       <ImageList
         id="imageList"
         sx={{ width: "100%", height: "auto" }}
-        variant="quilted"
         cols={cols}
         rowHeight={"auto"}
         gap={0}
       >
-        {/* {itemData.map((item) => ( */}
         {VIDEOS_TITLE_URL.map((vid, i) => (
           <ImageListItem
             key={i}
-            // cols={cols / 2 + (_.random(1024) % cols) / 2}
             cols={1}
             rows={1}
           >
